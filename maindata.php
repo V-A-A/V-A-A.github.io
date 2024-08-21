20ebfa6a55eff807dd611df09f2bfdd83a820150
V-A-A <vaa.16.12.20@gmail.com>
2024-08-21 23:50:45 +0530

    Add files via upload
    added 2nd half

---
+++ maindata.php
@@ -0,0 +1,36 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+//database connection
+
+$host = "localhost";
+$dbname = "portfolio_main";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (mysqli_connect_errno()) {
+    die("connection error: ". mysqli_connect_error());
+}
+
+$sql = "INSERT INTO contact (name, email , number,subject,  message)
+        VALUES (?,?,?,?,?)";
+
+$stmt = mysqli_stmt_init($conn);
+
+if (! mysqli_stmt_prepare($stmt, $sql)) {
+    die("". mysqli_error($conn));
+}
+mysqli_stmt_bind_param($stmt,"ssiss", $name, $email, $number, $subject, $message);
+
+mysqli_stmt_execute($stmt);
+
+echo "record saved."
+
+
+
+?>
